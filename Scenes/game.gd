344d51a79ef833b155ec4e764596b3023ac558eb
BigHBen <139195966+BigHBen@users.noreply.github.com>
2025-03-25 01:10:46 -0400

    Commit 3

--- Scenes/game.gd
+++ Scenes/game.gd
@@ -11,6 +11,9 @@ var game_paused : bool = false :
 	get:
 		return game_paused
 
+func _ready() -> void:
+	print("hello")
+
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_cancel"):
 		game_paused = !game_paused
